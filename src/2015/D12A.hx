f932285ef17204b0146b8ba1d06dfa33d0d2725f
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-12-17 11:58:31 +0800

    Add 2015/D12

---
+++ src/2015/D12A.hx
@@ -0,0 +1,52 @@
+package;
+
+import haxe.DynamicAccess;
+import sys.io.File;
+
+using Lambda;
+using StringTools;
+
+class D12A {
+	public static function main() {
+		process();
+	}
+
+	static function process() {
+		var sum = parseJson(haxe.Json.parse(input));
+		trace('Sum of numbers in document = $sum');
+	}
+
+	static function parseJson(dyn:Dynamic):Int {
+		var data:DynamicAccess<Dynamic> = dyn;
+		var sum = 0;
+		switch (Type.typeof(data)) {
+			case TInt:
+				sum += cast(data, Int);
+			case TClass(Array):
+				sum += parseArray(data);
+			case TObject:
+				sum += parseObject(data);
+			case _:
+				{}
+		}
+		return sum;
+	}
+
+	static function parseObject(dyn:Dynamic):Int {
+		var obj:DynamicAccess<Dynamic> = dyn;
+		var sum = 0;
+		for (k => v in obj)
+			sum += parseJson(v);
+		return sum;
+	}
+
+	static function parseArray(dyn:Dynamic):Int {
+		var array:Array<Dynamic> = dyn;
+		var sum = 0;
+		for (i in array)
+			sum += parseJson(i);
+		return sum;
+	}
+
+	static var input = File.getContent("D12.input");
+}
