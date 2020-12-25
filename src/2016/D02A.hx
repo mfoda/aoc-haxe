be55cd5bf18d2887e67acd06020ff6b7ba138fe3
mfoda <23182653+mfoda@users.noreply.github.com>
2020-12-25 15:23:56 +0800

    Add 2016/D02

---
+++ src/2016/D02A.hx
@@ -0,0 +1,46 @@
+package;
+
+import sys.io.File;
+
+using Lambda;
+using StringTools;
+
+class D02A
+{
+	static final input = File.getContent("D02.input");
+
+	public static function main()
+	{
+		var x = 1;
+		var y = 1;
+		var btnLayout = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+		var code = "";
+
+		function max(a, b)
+			return Std.int(Math.max(a, b));
+		function min(a, b)
+			return Std.int(Math.min(a, b));
+
+		for (line in input.split("\n"))
+		{
+			for (cmd in line.iterator())
+			{
+				switch (String.fromCharCode(cmd))
+				{
+					case "U":
+						y = max(0, y - 1);
+					case "R":
+						x = min(2, x + 1);
+					case "D":
+						y = min(2, y + 1);
+					case "L":
+						x = max(0, x - 1);
+					case c:
+						throw 'invalid cmd $c';
+				}
+			}
+			code += btnLayout[y][x];
+		}
+		Sys.println('Bathroom code is $code');
+	}
+}
---
+++ src/2016/D02B.hx
@@ -0,0 +1,58 @@
+package;
+
+import sys.io.File;
+
+using Lambda;
+using StringTools;
+
+class D02B
+{
+	static final input = File.getContent("D02.input");
+
+	static inline var NUL = "_";
+
+	public static function main()
+	{
+		var x = 0;
+		var y = 2;
+		var btnLayout = [
+			[NUL, NUL, "1", NUL, NUL],
+			[NUL, "2", "3", "4", NUL],
+			["5", "6", "7", "8", "9"],
+			[NUL, "A", "B", "C", NUL],
+			[NUL, NUL, "D", NUL, NUL]
+		];
+		var code = "";
+
+		function max(a, b)
+			return Std.int(Math.max(a, b));
+		function min(a, b)
+			return Std.int(Math.min(a, b));
+
+		for (line in input.split("\n"))
+		{
+			for (cmd in line.iterator())
+			{
+				switch (String.fromCharCode(cmd))
+				{
+					case "U":
+						var nxtRow = btnLayout[y - 1];
+						var nxtRowValid = nxtRow != null && nxtRow[x] != NUL;
+						y = nxtRowValid ? max(0, y - 1) : y;
+					case "D":
+						var nxtRow = btnLayout[y + 1];
+						var nxtRowValid = nxtRow != null && nxtRow[x] != NUL;
+						y = nxtRowValid ? min(btnLayout.length - 1, y + 1) : y;
+					case "R":
+						x = (btnLayout[y][x + 1] != NUL) ? min(btnLayout[y].length - 1, x + 1) : x;
+					case "L":
+						x = (btnLayout[y][x - 1] != NUL) ? max(0, x - 1) : x;
+					case c:
+						throw 'invalid cmd $c';
+				}
+			}
+			code += btnLayout[y][x];
+		}
+		Sys.println('Bathroom code is $code');
+	}
+}
