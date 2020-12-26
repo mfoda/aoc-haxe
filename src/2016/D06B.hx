66477eb6a9ba21b4d2035ce7fba9c075099ec426
mfoda <23182653+mfoda@users.noreply.github.com>
2020-12-26 12:58:11 +0800

    Add 2016/D06

---
+++ src/2016/D06B.hx
@@ -0,0 +1,34 @@
+package;
+
+import sys.io.File;
+
+using Lambda;
+using StringTools;
+
+class D06B {
+	static final input = File.getContent("D06.input");
+
+	public static function main() {
+		var letters = input.split("\n").map(x -> x.split(""));
+		var corrected = "";
+		for (i in 0...letters[0].length) {
+			var column = [for (row in letters) row[i]];
+			corrected += mostFreq(column);
+		}
+		Sys.println('Error-corrected version of message is $corrected');
+	}
+
+	static function mostFreq(letters:Array<String>):String {
+		var freq = new Map<String, Int>();
+		for (letter in letters)
+			if (freq.exists(letter))
+				freq[letter]++;
+			else
+				freq[letter] = 1;
+
+		var sorted = [for (k => v in freq.keyValueIterator()) {letter: k, freq: v}];
+		sorted.sort((a, b) -> a.freq < b.freq ? -1 : 1);
+
+		return sorted[0].letter;
+	}
+}
